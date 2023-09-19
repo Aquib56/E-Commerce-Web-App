eec46ecf9074cb74f823ad51c2a2d8dfa503983e
Aquib <aquibs732@gmail.com>
2023-09-19 17:45:12 +0530

    Implentated user registration with email based authentication

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from django.contrib import messages
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -39,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'ecommerceapp',
+    'authcart',
 ]
 
 MIDDLEWARE = [
@@ -113,6 +115,16 @@ USE_I18N = True
 
 USE_TZ = True
 
+# sending emails
+
+EMAIL_HOST='smtp.gmail.com'
+EMAIL_HOST_USER='aquibshaikh8390@gmail.com'
+EMAIL_HOST_PASSWORD='wlingpommjbxyjva'
+EMAIL_PORT=587
+EMAIL_USE_TLS=True
+EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
+
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
@@ -126,3 +138,7 @@ STATICFILES_DIRS=[
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MESSAGE_TAGS = {
+    messages.ERROR:'danger'
+}
