23bc609d8d02a72247e5791db721168006acd070
Aquib <aquibs732@gmail.com>
2023-09-26 17:34:06 +0530

    Functionality of adding,removing and clearing items from Cart

--- ecommerce/ecommerceapp/urls.py
+++ ecommerce/ecommerceapp/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('',views.index,name="index"),
     path('contact',views.contact,name="contact"),
     path('about',views.about,name="contact"),
+    path('checkout/',views.checkout,name="checkout"),
 ]
