eb59780b91be0c6bd8f3397708d9e46090665755
Aquib <aquibs732@gmail.com>
2023-09-28 20:15:29 +0530

    Payment integeration using PayTm

--- ecommerce/ecommerceapp/views.py
+++ ecommerce/ecommerceapp/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render,redirect
-from ecommerceapp.models import Contact,Product
+from ecommerceapp.models import Contact,Product,OrderUpdate,Orders
 from django.contrib import messages
 from math import ceil
+from ecommerceapp import keys
+from django.conf import settings
+MERCHANT_KEY=keys.MK
+import json
+from django.views.decorators.csrf import  csrf_exempt
+from PayTm import Checksum
 # Create your views here.
 def index(request):
 
@@ -78,3 +84,39 @@ def checkout(request):
         return render(request, 'paytm.html', {'param_dict': param_dict})
 
     return render(request, 'checkout.html')
+
+
+@csrf_exempt
+def handlerequest(request):
+    # paytm will send you post request here
+    form = request.POST
+    response_dict = {}
+    for i in form.keys():
+        response_dict[i] = form[i]
+        if i == 'CHECKSUMHASH':
+            checksum = form[i]
+
+    verify = Checksum.verify_checksum(response_dict, MERCHANT_KEY, checksum)
+    if verify:
+        if response_dict['RESPCODE'] == '01':
+            print('order successful')
+            a=response_dict['ORDERID']
+            b=response_dict['TXNAMOUNT']
+            rid=a.replace("ShopyCart","")
+           
+            print(rid)
+            filter2= Orders.objects.filter(order_id=rid)
+            print(filter2)
+            print(a,b)
+            for post1 in filter2:
+
+                post1.oid=a
+                post1.amountpaid=b
+                post1.paymentstatus="PAID"
+                post1.save()
+            print("run agede function")
+        else:
+            print('order was not successful because' + response_dict['RESPMSG'])
+    return render(request, 'paymentstatus.html', {'response': response_dict})
+
+
